461bc30e4a148f9b21e4e8be40aab23902367855
enrique-alvarez <enrique.alvarez@distilled.ie>
2018-08-03 16:45:21 +0100

    first commit

---
+++ src/main/java/ie/distilledsch/rest/application/MainApplication.java
@@ -0,0 +1,10 @@
+package ie.distilledsch.rest.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
